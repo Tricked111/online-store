a11b21616e2559713f0159a5e95a3809ded8c1d4
Tricked111 <Shift3114@gamil.com>
2022-12-09 13:55:52 +0100

    navbar update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,22 +5,39 @@ import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import { Link } from "react-router-dom";
+import Navbar from 'react-bootstrap/Navbar'
+import Container from 'react-bootstrap/Container'
+import { LinkContainer } from 'react-router-bootstrap'
 
 function App() {
   return (
     <BrowserRouter>
-    <div>
+    {/* header */}
+    <div className='d-flex flex-column site-container'>
       <header>
-        <Link to='/'>Marketpalace</Link>
+        <Navbar bg="dark" variant="dark">
+          <Container>
+            <LinkContainer to="/">
+              <Navbar.Brand>Marketplace</Navbar.Brand>
+            </LinkContainer>
+          </Container>
+        </Navbar>
       </header>
-      
+      {/* main */}
       <main>
-        <Routes>
-          <Route path="/product/:slug" element={<ProductScreen />} />
-          <Route path="/" element={<HomeScreen />} />
-        </Routes>
+        <Container>
+            <Routes>
+              <Route path="/product/:slug" element={<ProductScreen />} />
+              <Route path="/" element={<HomeScreen />} />
+            </Routes>
+        </Container>
       </main>
-    
+      {/* footer */}
+      <footer>
+        <div className='text-center'>TODO</div>
+      </footer>
+
+
     </div>
     </BrowserRouter>
   );
