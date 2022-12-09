3776df6dbaed57d9ebf59455c1438c7b1ac02ce4
Tricked111 <Shift3114@gamil.com>
2022-12-09 11:27:15 +0100

    add data

---
+++ frontend/src/data.js
@@ -0,0 +1,42 @@
+const data = {
+    products: [
+        {
+            name: 'Nike Slim shirt',
+            slug: 'nike-slim-shirt',
+            categoey: 'Shirts',
+            image: '/images/p1.jpg',
+            price: 120,
+            countInStock: 10,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'hight quality shirt',
+        },
+        {
+            name: 'Puma Slim shirt',
+            slug: 'puma-slim-shirt',
+            categoey: 'Shirts',
+            image: '/images/p2.jpg',
+            price: 120,
+            countInStock: 10,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'hight quality shirt',
+        },
+        {
+            name: 'Adidas Slim shirt',
+            slug: 'adidas-slim-shirt',
+            categoey: 'Shirts',
+            image: '/images/p3.jpg',
+            price: 120,
+            countInStock: 10,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'hight quality shirt',
+        },
+    ],
+};
+
+export default data;
