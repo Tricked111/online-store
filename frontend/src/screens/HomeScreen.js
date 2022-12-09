923123140eafbe2d729252538ce8d631112a6337
Tricked111 <Shift3114@gamil.com>
2022-12-09 13:39:36 +0100

    changes

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,22 +1,56 @@
 //import data from "../data";
 import { Link } from "react-router-dom";
-import { useState, useEffect } from 'react';
-import axios from 'axios'
+import { useState, useEffect, useReducer } from 'react';
+import axios from 'axios';
+import logger from 'use-reducer-logger'
+
+const reducer = (state,action) => {
+  switch(action.type){
+    case 'FETCH_REQUEST':
+      return {...state,loading: true};
+    case 'FETCH_SUCCUES':
+      return {...state,products: action.payload, loading: false};
+    case 'FETCH_FAIL':
+      return {...state,loading: false,error: action.payload};
+    default:
+      return state;
+    }
+}
 
 
 function HomeScreen(){
-  const [products,setProducts] = useState([]);
+  const [{loading,error,products},dispatch] = useReducer(logger(reducer),{
+    products: [],
+    loading: true,
+    error: '',
+  });
+  //const [products,setProducts] = useState([]);
   useEffect(() =>{
     const fetchData = async () => {
+
+      dispatch({type: 'FETCH_REQUEST'});
+      try{
+        const result = await axios.get('/api/products');
+        dispatch({type:'FETCH_SUCCUES',payload: result.data });
+      }catch(err){
+        dispatch({type: 'FETCH_FAIL',payload: err.message});
+      }
       const result = await axios.get('/api/products');
-      setProducts(result.data);
+      //setProducts(result.data);
     };
     fetchData();
   },[]);
     return <div>
         <h1>Featured products</h1>
         <div className="products">
-        {products.map(product => (
+
+        {loading? (
+          <div>loading...</div>
+        ) : error ? (
+          <div>{error}</div>
+        ) : (
+
+          products.map(product => (
           <div className="product" key={product.slug}>
             <Link to={`/product/${product.slug}`}>
              <img src={product.image} alt={product.name} />
@@ -29,6 +63,7 @@ function HomeScreen(){
             <button>Add to cart</button>
           </div>
           </div>))
+        )
         }
         </div>
     </div>;
