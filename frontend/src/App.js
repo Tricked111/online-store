98662162f754b93f45c350a693d8a27dfa9fac0e
Tricked111 <Shift3114@gamil.com>
2022-12-09 11:26:46 +0100

    add product list

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import data from "./data"
+
 
 function App() {
   return (
@@ -8,7 +10,23 @@ function App() {
         <a href='/'>Marketpalace</a>
       </header>
       <main>
-        list products
+        <h1>Featured products</h1>
+        <div className="products">
+        {data.products.map(product => (
+          <div className="product" key={product.slug}>
+            <a href={`/product/${product.slug}`}>
+             <img src={product.image} alt={product.name} />
+            </a>
+          <div className="product-info">
+            <a href={`/product/${product.slug}`}>
+            <p>{product.name}</p>
+            </a>
+            <p><strong>${product.price}</strong></p>
+            <button>Add to cart</button>
+          </div>
+          </div>))
+        }
+        </div>
       </main>
     </div>
   );
