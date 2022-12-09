26ead4cedfdfb8af459d6c106245de75959d7182
Tricked111 <Shift3114@gamil.com>
2022-12-09 13:11:59 +0100

    add proxy

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,11 +1,22 @@
-import data from "../data";
+//import data from "../data";
 import { Link } from "react-router-dom";
+import { useState, useEffect } from 'react';
+import axios from 'axios'
+
 
 function HomeScreen(){
+  const [products,setProducts] = useState([]);
+  useEffect(() =>{
+    const fetchData = async () => {
+      const result = await axios.get('/api/products');
+      setProducts(result.data);
+    };
+    fetchData();
+  },[]);
     return <div>
         <h1>Featured products</h1>
         <div className="products">
-        {data.products.map(product => (
+        {products.map(product => (
           <div className="product" key={product.slug}>
             <Link to={`/product/${product.slug}`}>
              <img src={product.image} alt={product.name} />
