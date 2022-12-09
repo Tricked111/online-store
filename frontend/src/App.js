0a543481390d579eb78df68bf2fd4bbc8bdf730f
Tricked111 <Shift3114@gamil.com>
2022-12-09 10:51:13 +0100

    create repo

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href='/'>Marketpalace</a>
+      </header>
+      <main>
+        list products
+      </main>
+    </div>
+  );
+}
+
+export default App;
