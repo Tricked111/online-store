c5c6dd5ae801f4962183c4556163cf53fb377da8
Tricked111 <Shift3114@gamil.com>
2022-12-09 12:15:01 +0100

    add link

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
-import data from "./data"
-
+import data from "./data";
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+import { Link } from "react-router-dom";
 
 function App() {
   return (
+    <BrowserRouter>
     <div>
       <header>
-        <a href='/'>Marketpalace</a>
+        <Link to='/'>Marketpalace</Link>
       </header>
+      
       <main>
-        <h1>Featured products</h1>
-        <div className="products">
-        {data.products.map(product => (
-          <div className="product" key={product.slug}>
-            <a href={`/product/${product.slug}`}>
-             <img src={product.image} alt={product.name} />
-            </a>
-          <div className="product-info">
-            <a href={`/product/${product.slug}`}>
-            <p>{product.name}</p>
-            </a>
-            <p><strong>${product.price}</strong></p>
-            <button>Add to cart</button>
-          </div>
-          </div>))
-        }
-        </div>
+        <Routes>
+          <Route path="/product/:slug" element={<ProductScreen />} />
+          <Route path="/" element={<HomeScreen />} />
+        </Routes>
       </main>
+    
     </div>
+    </BrowserRouter>
   );
 }
 
