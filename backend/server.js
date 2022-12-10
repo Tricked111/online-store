92510bc21ea257f7b7735958b0e476d4c7840160
Tricked111 <Shift3114@gamil.com>
2022-12-10 13:13:19 +0100

    fix

--- backend/server.js
+++ backend/server.js
@@ -18,15 +18,15 @@ mongoose.connect(process.env.MONGODB_URI).then(() => {
 
 
 const app = express();
+
 app.use('/api/seed', seedRouter);
 app.use('/api/products', productRouter);
+
+
 
 app.get('/api/products',(req,res) => {
     res.send(data.products)
 });
-
-
-
 
 const port = process.env.PORT || 5000;
 app.listen(port,() => {
